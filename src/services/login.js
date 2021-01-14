ca8dc98f512a27da123c5e91f05b7265adefd42d
Rohit Pradhan <rohit.p.pradhan88@gmail.com>
2021-01-13 21:57:05 -0500

    Included parameters for loginService.login in documentation

--- src/services/login.js
+++ src/services/login.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 const baseUrl = '/api/login'
 
 /**
-	Logs in the user.
+	Logs in the user in with the given credentials. Returns 401 if
+	the user is not successfully authenticated.
 */
 async function login(credentials) {
 	const response = await axios.post(baseUrl, credentials)
